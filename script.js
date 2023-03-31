e9490542abeddfb0c2ab9408c4e33fc5278cfa06
Pedro Mattos <pedromattospoli@gmail.com>
2023-03-31 01:51:15 -0300

    Adicionado o calculo do valor total

--- script.js
+++ script.js
@@ -1,10 +1,32 @@
+let valorTotal = 0;
+let valorPrincipal = 0;
+let valorBebida = 0;
+let valorSobremesa = 0;
+
+
 function Check( container, seletor){
-    const junta = container + " " + '.check'
-    const Anterior = document.querySelector(junta);
+    const Anterior = document.querySelector(container + ' .check');
     if(Anterior !== null){
         Anterior.classList.remove('check');
     }
 
     const PratoSelecionado = document.querySelector(seletor);
     PratoSelecionado.classList.add('check');
+
+    const tagpreco = document.querySelector(seletor +' .preco');
+    console.log(tagpreco);
+    preco = parseFloat(tagpreco.innerHTML.replace(',','.'));
+    console.log(preco);
+    
+    if(container === ".menu-pratos-principais"){
+        valorPrincipal = preco;
+    } else if (container === ".menu-bebidas"){
+        valorBebida = preco;
+    } else{
+        valorSobremesa = preco;
+    }
+    
+   valorTotal = valorPrincipal + valorBebida + valorSobremesa;
+   console.log(valorTotal);
+
 }
